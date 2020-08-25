7b05d15d102fe5cac6400d46f0ba7d6e641b601b
Diego V <diego.vgn@hotmail.com>
2020-08-25 16:39:44 +0200

    v4
    Mouse button hold added

--- Fungopaint.py
+++ Fungopaint.py
@@ -17,16 +17,24 @@ def main():
     mouse = pygame.mouse
     drawing = False
     last_pos = pygame.mouse.get_pos()
+    clock = pygame.time.Clock()
 
+    z = 0
     # Event loop
     while True:
-        left_pressed = mouse.get_pressed()
+        clock.tick(60)
+        x,y = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.Quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                pygame.draw.line(screen, BLACK, last_pos, pygame.mouse.get_pos(), 1)
-        pygame.display.update()
+                #pygame.draw.line(screen, BLACK, last_pos, pygame.mouse.get_pos())
+                z = 1
+            elif event.type == MOUSEBUTTONUP:
+                z = 0
 
+            if z == 1:
+                pygame.draw.line(screen, BLACK, last_pos, pygame.mouse.get_pos())
+        pygame.display.update()
 if __name__ == '__main__': main()
